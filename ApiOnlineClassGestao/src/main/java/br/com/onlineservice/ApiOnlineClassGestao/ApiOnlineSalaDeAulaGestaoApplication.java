27b230753482a63c9fa0292c3f757d1af00f92d5
Mailon Bruno <113206774+mailoncampos@users.noreply.github.com>
2022-09-12 21:59:39 -0400

    Criando o projeto da API Online Class Gestão

---
+++ ApiOnlineClassGestao/src/main/java/br/com/onlineservice/ApiOnlineClassGestao/ApiOnlineSalaDeAulaGestaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.onlineservice.ApiOnlineClassGestao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiOnlineSalaDeAulaGestaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiOnlineSalaDeAulaGestaoApplication.class, args);
+	}
+
+}
---
+++ ApiOnlineClassGestao/src/test/java/br/com/onlineservice/ApiOnlineClassGestao/ApiOnlineSalaDeAulaGestaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.onlineservice.ApiOnlineClassGestao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiOnlineSalaDeAulaGestaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
